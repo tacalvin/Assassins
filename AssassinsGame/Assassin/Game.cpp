05a052e9bc5558dcbaf74ec3c3777fa4a755d01e
Calvin <tacalvin123@gmail.com>
2015-11-08 00:18:32 -0800

    sdaf

--- AssassinsGame/Assassin/Game.cpp
+++ AssassinsGame/Assassin/Game.cpp
@@ -9,7 +9,14 @@ using namespace std;
 Game::Game(string sessionName)
 {
 	ifstream game(sessionName + ".txt");
-	if(game.
+	if(game.fail())
+	{
+	cout<<"Error Game does not exist please contact system admin"<<endl
+	return -1;
+
+	
+	}
+	loadSession(string sessionName);
 }
 
 string Game::createSession(string sessionName)
@@ -17,8 +24,11 @@ string Game::createSession(string sessionName)
 	return sessionName;
 }
 
+//
 bool Game::loadSession(string sessionName)
 {
+	ifstream game(sessionName + ".txt");
+	while(
 	return false;
 }
 
