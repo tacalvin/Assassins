0daeec9ea238bb5908029b51437190eff3900d2f
mdrush <mrusheen@gmail.com>
2015-11-07 22:27:24 -0800

    Added more SQL

--- post.php
+++ post.php
@@ -3,28 +3,49 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
+// Including the game logic
+//require 'GameManager.php';
+
 //Get data from submitted form
 $name = $_POST["name"];
 $email = $_POST["email"];
 $password = $_POST["password"];
 
-
+$pin = 1; // FIXME: generate random pin
 
 //Helps prevent SQL Injection Attacks
 //$name_es = sqlite_escape_string($name);
 //$email_es = sqlite_escape_string($email);
 if (!empty($name)) {
 
-   $dbhandle = new SQLite3('db/game1.db');
+    $dbhandle = new SQLite3('db/game1.db');
 
-   //$dbhandle->exec('CREATE TABLE game (name(255), email(255), pin(255))');
+   // Check if there is a table already
+	$val = $dbhandle->querySingle('select 1 from `game` LIMIT 1');
+	if($val !== FALSE)
+	{
+ 	$dbhandle->exec('CREATE TABLE game (name TEXT, email TEXT, pin INTEGER)');
+	}
    
-   $stm = "INSERT INTO game (name, email, pin) VALUES('$name', '$email', 0)";
+   if(!empty($name)){
+   	// User is registering
+   // Add new regestered player's info to the table
+   $stm = "INSERT INTO game (name, email, pin) VALUES('$name', '$email', '$pin')";
    $dbhandle->exec($stm);
-
+	}
+	else {
+		// User is logging in, they want their pin
+		$yourpin = $dbhandle->querySingle("SELECT pin FROM game WHERE email='$email'");
+		echo "Your pin is: " . $yourpin;	
+		echo "<input type="submit" name="kill" value="Kill"></br>"
+	}
+   // Thank user for logging in / registering
    	echo "Welcome " . $name;
 
+   	// Helps with debugging :)
 	echo "You entered: " . $name . " " . $email . " " . $password;
+	
+	// They need to know their pin to play
 	$yourpin = $dbhandle->querySingle("SELECT pin FROM game WHERE email='$email'");
 	echo "Your pin is: " . $yourpin;
 }
